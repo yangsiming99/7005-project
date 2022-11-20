dcd3ba4bdbf4d013087813f4ba3055cae28eda73
Wayne <wsyccf@gmail.com>
2022-11-20 12:01:39 -0800

    init window

--- client.py
+++ client.py
@@ -1,15 +1,20 @@
 import getopt
+import math
 import sys
 import socket
 import json
+import threading
 from enum import Enum
 
 BUFFER_SIZE = 1024
+SEGMENT_SIZE = 900
 WINDOW_SIZE = 100
 
-HOST = "192.168.1.16"
+HOST = "127.0.0.1"
 PORT = 5000
-FILE = "test.txt"
+FILE = ""
+
+TIME_OUT = 2000
 
 
 class WindowType(Enum):
@@ -17,23 +22,100 @@ class WindowType(Enum):
     SEND_NOT_ACKED_YET = 2
     AVALIABLE_NOT_SEND_YET = 3
     DISABLED_NOT_SEND_YET = 4
-    INIT = 5
 
 
-window = [{
-    type: WindowType.INIT,
-    "data": None
-}] * WINDOW_SIZE
+def update_to_avaliable(data):
+    data["type"] = WindowType.AVALIABLE_NOT_SEND_YET
+    return data
+
+
+def update_to_disable(data):
+    data["type"] = WindowType.DISABLED_NOT_SEND_YET
+    return data
+
+
+def update_to_acked(data):
+    data["type"] = WindowType.SEND_ACKED
+    return data
+
+
+def update_to_send(data):
+    data["type"] = WindowType.SEND_NOT_ACKED_YET
+    return data
 
 
 def main():
+    init_message = {
+        "file": FILE != "",
+        "segment_size": SEGMENT_SIZE,
+        "total_data_size": 0,
+        "buffer_size": BUFFER_SIZE,
+        "total_segments": 0
+    }
+
+    server_info = {
+        "window_size": 100
+    }
+
+    windows = []
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((HOST, PORT))
-        with open(FILE, "rb") as file:
-            send_data = file.read(BUFFER_SIZE)
-            while send_data:
-                sock.sendall(send_data)
-                send_data = file.read(BUFFER_SIZE)
+        sock.settimeout(TIME_OUT)
+        if init_message["file"]:
+            with open(FILE, "r", encoding="utf-8") as file:
+                data = file.read()
+        else:
+            data = input('>')
+
+        init_message["total_data_size"] = len(data)
+        result = init_window(data, init_message["total_data_size"])
+
+        init_message["total_segments"] = result["total_segments"]
+        window_buffer = result["window"]
+
+        print(init_message)
+
+        sock.sendall(json.dumps(init_message).encode('utf-8'))
+        server_info = json.loads(sock.recv(BUFFER_SIZE).decode('utf-8'))
+        max_window_size = server_info["max_window_size"]
+        # update the max server window size to avaliable
+        window_buffer = list(map(update_to_avaliable, window_buffer[0:max_window_size]))
+
+        # print(window_buffer)
+        # sock.setblocking(False)
+        for index, each in enumerate(window_buffer):
+            if each["type"] == WindowType.AVALIABLE_NOT_SEND_YET:
+                thread = threading.Thread(target=thread_send, args=(sock, each, index))
+                thread.start()
+                # thread.join()
+                # thread_send(sock, each, index)
+        ack = json.loads(sock.recv(BUFFER_SIZE).decode('utf-8'))
+        print(ack)
+
+
+def thread_send(sock, data, index):
+    sock.sendall(json.dumps({
+        "sequence": index,
+        "data": data["data"]
+    }).encode('utf-8'))
+    data["type"] = WindowType.SEND_NOT_ACKED_YET
+
+
+def init_window(data, length):
+    window = []
+    loop_times = math.ceil(length / SEGMENT_SIZE)
+
+    for i in range(loop_times):
+        window.append({
+            "type": WindowType.DISABLED_NOT_SEND_YET,
+            "sequence": i,
+            "data": data[i * SEGMENT_SIZE:i * SEGMENT_SIZE + SEGMENT_SIZE]
+        })
+    return {
+        "window": window,
+        "total_segments": loop_times
+    }
 
 
 def help_msg():
--- server.py
+++ server.py
@@ -1,11 +1,30 @@
 import getopt
 import sys
 import socket
+import json
+import threading
+from enum import Enum
 
 BACK_LOG = 5
 BUFFER_SIZE = 1024
+WINDOW_SIZE = 1000
 
 PORT = 5000
+
+
+class WindowType(Enum):
+    RECV_ACKED = 1
+    RECV_NOT_YET_ACK = 2
+    AVALIABLE = 3
+
+
+def thread_read(conn, sender_buffer_size, window_buffer):
+    data = json.loads(conn.recv(sender_buffer_size).decode('utf-8'))
+    window_buffer[data["sequence"]]["data"] = data["data"]
+    print(data["data"])
+    conn.sendall(json.dumps({
+        "sequence": data["sequence"] + 1
+    }).encode('utf-8'))
 
 
 def main():
@@ -15,13 +34,24 @@ def main():
         print("Waiting for client")
         while True:
             conn, from_addr = sock.accept()
+            window_buffer = [{
+                "type": WindowType.AVALIABLE,
+            }] * WINDOW_SIZE
+            sender_buffer_size = 1024
+            total_segments = 0
             print(f"Connection from {from_addr} has been established")
             with conn:
-                with open("test.txt", "wb") as file:
-                    data = conn.recv(BUFFER_SIZE)
-                    while data:
-                        file.write(data)
-                        data = conn.recv(BUFFER_SIZE)
+                sender_init_message = json.loads(conn.recv(BUFFER_SIZE).decode('utf-8'))
+                sender_buffer_size = sender_init_message["buffer_size"]
+                total_segments = sender_init_message["total_segments"]
+                sender_segment_size = sender_init_message["segment_size"]
+
+                conn.sendall(json.dumps({
+                    "max_window_size": WINDOW_SIZE,
+                }).encode('utf-8'))
+
+                thread = threading.Thread(target=thread_read, args=(conn, sender_buffer_size, window_buffer))
+                thread.start()
 
 
 def help_msg():
